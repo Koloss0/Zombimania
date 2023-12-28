85428181003f462dbe7792881a36049f3f41e511
Logan Furedi <furedil@myumanitoba.ca>
2023-12-28 17:48:35 -0600

    Test Framerate of 2000 Sprites

--- src/states/main_menu.c
+++ src/states/main_menu.c
@@ -1,11 +1,44 @@
 #include "states.h"
 #include "gfx/gfx.h"
+#include "math/math.h"
+#include "gfx/sprite2d.h"
+#include "config.h"
 
 #include <stdbool.h>
+#include <stdio.h>
+
+struct Sprite
+{
+	double x, y;
+};
+
+#define NUM_SPRITES SPRITES_MAX
+
+static struct Sprite sprites[NUM_SPRITES];
+static int frame = 0;
 
 // called once at the start of the program.
 void mm_init()
-{}
+{
+	for (int i = 0; i < NUM_SPRITES; i++)
+	{
+		struct Sprite sprite;
+
+		//sprite.x = math_randf_gaussian((double)VIEWPORT_WIDTH / 2.0, (double)VIEWPORT_WIDTH / 16.0);
+		//sprite.y = math_randf_gaussian((double)VIEWPORT_HEIGHT / 2.0, (double)VIEWPORT_HEIGHT / 16.0);
+
+		double rx = (double)VIEWPORT_WIDTH / 2.0;
+		double ry = (double)VIEWPORT_HEIGHT / 2.0;
+		double x = (double)VIEWPORT_WIDTH / 2.0;
+		double y = (double)VIEWPORT_HEIGHT / 2.0;
+		sprite.x = math_randf_range(x - rx, x + rx);
+		sprite.y = math_randf_range(y - ry, y + ry);
+
+		sprites[i] = sprite;
+	}
+
+	frame = 0;
+}
 
 // called once before the program exits.
 void mm_destroy()
@@ -26,11 +59,20 @@ void mm_update(double delta)
 
 	gfx_background(0.0f, 0.0f, 0.0f);
 
-	gfx_sprite2d(30.0f, 55.0f, 25.0f, 25.0f, (TextureRect){51, 51, 95, 95});
-	gfx_sprite2d(50.0f, 50.0f, 25.0f, 25.0f, (TextureRect){332, 51, 95, 95});
-	gfx_sprite2d(70.0f, 45.0f, 25.0f, 25.0f, (TextureRect){508, 51, 95, 95});
+	for (int i = 0; i < NUM_SPRITES; i++)
+	{
+		sprites[i].x = (float)math_fmod(sprites[i].x + 1.0, (double)VIEWPORT_WIDTH);
+		gfx_sprite2d((float)sprites[i].x, (float)sprites[i].y, 25.0f, 25.0f, (TextureRect){51, 51, 95, 95});
+	}
 
 	gfx_end();
+
+	if (frame % 1000 == 0)
+	{
+		printf("frame: %d\n", frame);
+	}
+
+	frame++;
 }
 
 // called when a key is either pressed, repeated (held down), or released.
