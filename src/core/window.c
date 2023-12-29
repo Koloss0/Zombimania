a888a1754ec41a4023c598494d06eb33a72508b3
Logan Furedi <kolosso@pm.me>
2023-12-29 13:11:04 -0600

    Fix: Fix Incorrect Values For Key Input Callback

--- src/core/window.c
+++ src/core/window.c
@@ -194,7 +194,7 @@ static void resize_callback(GLFWwindow* window, int width, int height)
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
 	// TODO: use callback instead.
-	fsm_key_input(key, scancode, action, mods);
+	fsm_key_input(key, action, scancode, mods);
 }
 
 static void cursor_position_callback(GLFWwindow* window, double x, double y)
