7bb8729e776909128467a26138909ea3a2a5d8c4
Logan Furedi <furedil@myumanitoba.ca>
2024-01-02 13:18:17 -0600

    Fix: Add Functions for Game State

---
+++ src/states/game.c
@@ -0,0 +1,31 @@
+#include "states.h"
+
+#include <stdbool.h>
+
+// called once at the start of the program.
+void game_init()
+{}
+
+// called once before the program exits.
+void game_destroy()
+{}
+
+// called whenever the state is entered.
+void game_enter()
+{}
+
+// called whenever the state exits.
+void game_exit()
+{}
+
+// called every frame when the state is active.
+void game_update(double delta)
+{}
+
+// called when a key is either pressed, repeated (held down), or released.
+void game_key_input(int key, int action, int scancode, int mods)
+{}
+
+// called when a mouse button is pressed or released.
+void game_mouse_button_input(int button, bool pressed, int mods)
+{}
